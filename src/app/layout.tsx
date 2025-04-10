93119b69b1b1aa27377e86e56c31d2672542d1b1
Deepthi <deepthidaggubati5@gmail.com>
2025-04-10 15:11:04 +0530

    authentication done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,16 @@ import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toaster'
 
 export const metadata = {
-  title: 'Breadit',
+  title: 'College-Connect',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
 const inter = Inter({
-  subsets: ['latin'],})
+  subsets: ['latin']
+})
 
 export default function RootLayout({
   children,
@@ -17,11 +19,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en' className = {cn('bg-white text-slate-900 antialiased light' , inter.className)}>
+    <html
+     lang='en' 
+     className = {cn(
+      'bg-white text-slate-900 antialiased light' ,
+       inter.className
+       )}>
       <body className='min-h-screen pt-12 bg-slate-50 antiliased'>
         <Navbar />
         <div className='container max-w-7xl mx-auto h-full pt-12'></div>
-      {children}</body>
+      {children}
+      <Toaster />
+      </body>
     </html>
   )
 }
